e473309d98676ea601efaf8a3ea6172856456bc6
Berenger <berenger.recoules@gmail.com>
2017-02-20 18:43:23 +0100

    set invertMask editable

--- core/src/net/mgsx/rainstick/components/InvertMask.java
+++ core/src/net/mgsx/rainstick/components/InvertMask.java
@@ -3,8 +3,9 @@ package net.mgsx.rainstick.components;
 import com.badlogic.ashley.core.Component;
 import com.badlogic.ashley.core.ComponentMapper;
 
+import net.mgsx.game.core.annotations.EditableComponent;
 import net.mgsx.game.core.annotations.Storable;
-
+@EditableComponent
 @Storable("rainstick.mask-invert")
 public class InvertMask implements Component
 {
