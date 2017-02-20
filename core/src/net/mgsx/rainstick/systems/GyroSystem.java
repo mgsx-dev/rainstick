188ec77d938870fbc5981b9031b068f2a7e7ec49
Germain AUBERT <germain.aubert@gmail.com>
2017-02-20 18:59:52 +0100

    enable gyro only with capable device

--- core/src/net/mgsx/rainstick/systems/GyroSystem.java
+++ core/src/net/mgsx/rainstick/systems/GyroSystem.java
@@ -2,6 +2,7 @@ package net.mgsx.rainstick.systems;
 
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Peripheral;
 
 import net.mgsx.game.core.GamePipeline;
 import net.mgsx.game.plugins.box2d.systems.Box2DWorldSystem;
@@ -14,9 +15,10 @@ public class GyroSystem extends EntitySystem
 	
 	@Override
 	public void update(float deltaTime) {
-		float gx = Gdx.input.getAccelerometerY();
-		float gy = -Gdx.input.getAccelerometerX();
-		
-		getEngine().getSystem(Box2DWorldSystem.class).gravity.set(gx, gy);
+		if(Gdx.input.isPeripheralAvailable(Peripheral.Accelerometer)){
+			float gx = Gdx.input.getAccelerometerY();
+			float gy = -Gdx.input.getAccelerometerX();
+			getEngine().getSystem(Box2DWorldSystem.class).gravity.set(gx, gy);
+		}
 	}
 }
