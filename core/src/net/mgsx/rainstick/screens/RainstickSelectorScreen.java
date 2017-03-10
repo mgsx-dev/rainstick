6bdddf0d6645524be9839da9d20e14de2f2cd217
Germain AUBERT <germain.aubert@gmail.com>
2017-03-10 15:34:22 +0100

    remove label on selection screen

--- core/src/net/mgsx/rainstick/screens/RainstickSelectorScreen.java
+++ core/src/net/mgsx/rainstick/screens/RainstickSelectorScreen.java
@@ -2,16 +2,11 @@ package net.mgsx.rainstick.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.assets.AssetManager;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.freetype.FreetypeFontLoader.FreeTypeFontLoaderParameter;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Label.LabelStyle;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
@@ -35,11 +30,6 @@ public class RainstickSelectorScreen extends StageScreen
 		this.assets = assets;
 		this.screenManager = screenManager;
 		
-		FreeTypeFontLoaderParameter p = new FreeTypeFontLoaderParameter();
-		p.fontParameters.size = 60;
-		p.fontFileName = "anchor-steam-nf/AnchorSteamNF.ttf";
-		assets.load("font-x60.ttf", BitmapFont.class, p);
-		
 		rainsticks = new Json().fromJson(Array.class, Rainstick.class, Gdx.files.internal("rainsticks.json"));
 		
 		for(Rainstick rainstick : rainsticks){
@@ -49,18 +39,10 @@ public class RainstickSelectorScreen extends StageScreen
 
 	@Override
 	public void show() {
-		BitmapFont font = assets.get("font-x60.ttf", BitmapFont.class);
 		
 		Table main = new Table(skin);
 		main.defaults().pad(10);
 		
-		LabelStyle style = new LabelStyle();
-		style.font = font;
-		style.fontColor = Color.BLACK;
-		Label label = new Label("Choose a Rainstick", style);
-		
-		main.add(label).row();
-
 		for(final Rainstick rainstick : rainsticks)
 		{
 			ImageButton bt = new ImageButton(new TextureRegionDrawable(new TextureRegion(assets.get(rainstick.preview, Texture.class))));
