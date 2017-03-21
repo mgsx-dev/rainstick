1c92ef567e3410fd417e4c277a472beeeb3b09ad
Berenger <berenger.recoules@gmail.com>
2017-03-21 15:36:11 +0100

    add ripple impact shader

---
+++ android/assets/shaders/impact-ripple-fragment.glsl
@@ -0,0 +1,37 @@
+#ifdef GL_ES
+#define LOWP lowp
+precision mediump float;
+#else
+#define LOWP
+#endif
+
+varying vec2 v_texCoords;
+uniform sampler2D u_texture;
+uniform float u_time;
+varying vec2 v_world;
+varying vec4 v_color;
+
+
+void main() {
+
+    vec4 color = texture2D(u_texture, v_texCoords);
+
+    float waveStrength = 0.15;
+    float frequency = 60.0;
+    float waveSpeed = 0.75;
+    vec4 sunlightColor = vec4(1.0,0.91,0.75,0.05);
+    float sunlightStrength = 1.0;
+    float centerLight = 0.2;
+    float oblique = 0.25; 
+          
+    vec2 uv = v_texCoords / vec2(v_world.x, v_world.y);
+    float modifiedTime = u_time * waveSpeed;
+    float distance = 1.0 -length(v_texCoords);
+    float multiplier = (distance < 1.0) ? ((distance-1.0)*(distance-1.0)) : 0.2;
+    float addend = (sin(frequency*distance-modifiedTime)+centerLight) * waveStrength ;
+    vec2 newTexCoord = uv+ addend*oblique;    
+    
+    vec4 colorToAdd = sunlightColor * sunlightStrength * addend;
+
+    gl_FragColor = vec4(color.r,color.g,color.b,distance * v_color.a)+colorToAdd;
+}
